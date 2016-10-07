9abcae92398a3d5cc51423bc7c12228b92f7fcbc
Travis Sanderson <travis.sanderson@webfilings.com>
2016-10-07 10:09:14 -0500

    Expand the example app to facilitate easily testing other navigator string values

--- example/main.dart
+++ example/main.dart
@@ -1,7 +1,16 @@
 import 'dart:html';
+import 'package:platform_detect/src/navigator.dart';
+import 'package:platform_detect/src/browser.dart';
+import 'package:platform_detect/src/operating_system.dart';
 import 'package:platform_detect/platform_detect.dart';
 
 main() {
+  _parseCurrentBrowser();
+  ButtonElement evaluate = querySelector('#evaluate-test');
+  evaluate.onClick.listen((_) => _parseTestValues());
+}
+
+void _parseCurrentBrowser() {
   document.querySelector('#current-browser').text = browser.name;
   document.querySelector('#current-vendor').text = window.navigator.vendor;
   document.querySelector('#current-appVersion').text = window.navigator.appVersion;
@@ -23,4 +32,22 @@ main() {
   document.querySelector('#current-version').text = browser.version.toString();
 
   document.querySelector('#current-os').text = operatingSystem.name;
+}
+
+void _parseTestValues() {
+  InputElement testVendor = querySelector('#test-vendor');
+  InputElement testAppVersion = querySelector('#test-appVersion');
+  InputElement testAppName = querySelector('#test-appName');
+  InputElement testUserAgent = querySelector('#test-userAgent');
+  var navigator = new TestNavigator();
+  navigator.vendor = testVendor.value.trim();
+  navigator.appVersion = testAppVersion.value.trim();
+  navigator.appName = testAppName.value.trim();
+  navigator.userAgent = testUserAgent.value.trim();
+  Browser.navigator = navigator;
+  OperatingSystem.navigator = navigator;
+
+  querySelector('#test-browser-name').text = browser.name;
+  querySelector('#test-browser-version').text = browser.version.toString();
+  querySelector('#test-os-name').text = operatingSystem.name;
 }
