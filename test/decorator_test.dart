3517b124f241f23e2d4f15ae905012c63d49617b
Aaron Lademann <aaron.lademann@workiva.com>
2017-04-10 07:55:19 -0700

    Ensure dupe features are removed

--- test/decorator_test.dart
+++ test/decorator_test.dart
@@ -11,83 +11,168 @@ void main() {
   group('root node CSS class injection', () {
     Element fakeRootNode;
     List calls;
-    CssClassSet fakeRootNodeClasses;
-    Feature consumerFeature;
 
     void callback() {
       calls.add('callback');
     }
 
-    setUp(() {
+    void sharedSetup() {
       calls = [];
       fakeRootNode = new DivElement();
-      consumerFeature =
-          new Feature('canvas', new CanvasElement().context2D != null);
-      decorateRootNodeWithPlatformClasses(
-          features: [consumerFeature],
-          rootNode: fakeRootNode,
-          callback: callback);
-      fakeRootNodeClasses = fakeRootNode.classes;
-    });
+    }
 
     tearDown(() {
+      calls = null;
       fakeRootNode = null;
-      consumerFeature = null;
-      fakeRootNodeClasses = null;
     });
 
-    test('should only happen once', () {
-      expect(calls, orderedEquals(['callback']));
+    group('', () {
+      setUp(() {
+        sharedSetup();
 
-      decorateRootNodeWithPlatformClasses(
-          rootNode: fakeRootNode, callback: callback);
-
-      expect(calls, orderedEquals(['callback']));
-    });
-
-    test('should identify the operating system', () {
-      expect(fakeRootNodeClasses,
-          contains('os-${nameToClassName(operatingSystem.name)}'));
-    });
-
-    group('should identify the browser', () {
-      test('', () {
-        expect(fakeRootNodeClasses,
-            contains('ua-${nameToClassName(browser.name)}'));
+        decorateRootNodeWithPlatformClasses(
+            rootNode: fakeRootNode, callback: callback);
       });
 
-      test('major version', () {
-        expect(
-            fakeRootNodeClasses,
-            contains(
-                'ua-${nameToClassName(browser.name)}${browser.version.major}'));
+      test('should only happen once', () {
+        expect(calls, orderedEquals(['callback']));
+
+        decorateRootNodeWithPlatformClasses(
+            rootNode: fakeRootNode, callback: callback);
+
+        expect(calls, orderedEquals(['callback']));
       });
 
-      test('major +$decoratedNextVersionCount version range', () {
-        for (var i = nextVersion;
-            i < nextVersion + decoratedNextVersionCount;
-            i++) {
-          expect(fakeRootNodeClasses,
-              contains('ua-lt-${nameToClassName(browser.name)}$i'));
-        }
+      test('should identify the operating system', () {
+        expect(fakeRootNode.classes,
+            contains('os-${nameToClassName(operatingSystem.name)}'));
       });
-    });
 
-    group('should identify feature support:', () {
-      // TODO: Set up saucelabs and run these tests on actual browsers on which we can assert whether
-      // our baked-in feature detection is accurate
-      test('default features', () {
-        for (var i = 0; i < defaultFeatureCssClassDecorators.length; i++) {
+      group('should identify the browser', () {
+        test('', () {
+          expect(fakeRootNode.classes,
+              contains('ua-${nameToClassName(browser.name)}'));
+        });
+
+        test('major version', () {
           expect(
-              fakeRootNodeClasses,
-              contains(matches(new RegExp(
-                  '(no-)*${defaultFeatureCssClassDecorators[i].name}'))));
+              fakeRootNode.classes,
+              contains(
+                  'ua-${nameToClassName(browser.name)}${browser.version.major}'));
+        });
+
+        test('major +$decoratedNextVersionCount version range', () {
+          for (var i = nextVersion;
+              i < nextVersion + decoratedNextVersionCount;
+              i++) {
+            expect(fakeRootNode.classes,
+                contains('ua-lt-${nameToClassName(browser.name)}$i'));
+          }
+        });
+      });
+    });
+
+    // TODO: Set up saucelabs and run these tests on actual browsers on which we can assert whether
+    // our baked-in feature detection is accurate
+    group('should identify feature support:', () {
+      void assertFakeRootNode() {
+        if (fakeRootNode == null) {
+          throw new AssertionError(
+              '`fakeRootNodeClasses` must be set before calling `verifyDistinctFeatureCssClasses`.');
         }
+      }
+
+      void verifyDistinctFeatureCssClasses(List<Feature> features) {
+        assertFakeRootNode();
+
+        String allCssClasses = fakeRootNode.classes.toString();
+        List<String> featureCssClasses =
+            getFeatureSupportClasses(features).split(' ');
+
+        for (var i = 0; i < featureCssClasses.length; i++) {
+          var affirmativeClassName = features[i].name;
+          var negationClassName =
+              '$featureSupportNegationClassPrefix-${features[i].name}';
+
+          expect(allCssClasses.allMatches(affirmativeClassName),
+              hasLength(lessThan(2)));
+          expect(allCssClasses.allMatches(negationClassName),
+              hasLength(lessThan(2)));
+        }
+      }
+
+      void verifyFeatureCssClasses(List<Feature> features) {
+        assertFakeRootNode();
+
+        for (var i = 0; i < features.length; i++) {
+          // 1. Ensure that its there
+          expect(
+              fakeRootNode.classes,
+              contains(matches(new RegExp(
+                  '($featureSupportNegationClassPrefix)*${features[i].name}'))));
+        }
+
+        // 2. Ensure that there is only one
+        verifyDistinctFeatureCssClasses(features);
+      }
+
+      setUp(() {
+        sharedSetup();
       });
 
-      test('custom feature provided by the consumer', () {
-        expect(fakeRootNodeClasses,
-            contains(matches(new RegExp('(no-)*${consumerFeature.name}'))));
+      test('default features', () {
+        decorateRootNodeWithPlatformClasses(
+            rootNode: fakeRootNode, callback: callback);
+
+        verifyFeatureCssClasses(defaultFeatureCssClassDecorators);
+      });
+
+      group('custom features provided by the consumer', () {
+        Feature uniqueConsumerFeature =
+            new Feature('canvas', new CanvasElement().context2D != null);
+        List<Feature> consumerFeaturesThatContainsNoDefaults = [
+          uniqueConsumerFeature
+        ];
+        List<Feature> consumerFeaturesThatMatchesDefaults =
+            new List.from(defaultFeatureCssClassDecorators);
+        List<Feature> consumerFeaturesThatContainsDefaults =
+            new List.from(defaultFeatureCssClassDecorators)
+              ..add(uniqueConsumerFeature);
+
+        test(
+            'when none of them are found within `defaultFeatureCssClassDecorators`',
+            () {
+          decorateRootNodeWithPlatformClasses(
+              features: consumerFeaturesThatContainsNoDefaults,
+              rootNode: fakeRootNode,
+              callback: callback);
+
+          verifyFeatureCssClasses(
+              new List.from(defaultFeatureCssClassDecorators)
+                ..addAll(consumerFeaturesThatContainsNoDefaults));
+        });
+
+        test(
+            'when all of them are found within `defaultFeatureCssClassDecorators`',
+            () {
+          decorateRootNodeWithPlatformClasses(
+              features: consumerFeaturesThatMatchesDefaults,
+              rootNode: fakeRootNode,
+              callback: callback);
+
+          verifyFeatureCssClasses(consumerFeaturesThatMatchesDefaults);
+        });
+
+        test(
+            'when some of them are found within `defaultFeatureCssClassDecorators`',
+            () {
+          decorateRootNodeWithPlatformClasses(
+              features: consumerFeaturesThatContainsDefaults,
+              rootNode: fakeRootNode,
+              callback: callback);
+
+          verifyFeatureCssClasses(consumerFeaturesThatContainsDefaults);
+        });
       });
     });
   });
