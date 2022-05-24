9cf8f24a7eb84d0f25112b15e67fbc10bf471905
wesleybalvanz-wf <wesley.balvanz@workiva.com>
2022-05-24 15:28:30 -0500

    Use global version of w_test_tools

--- run_tests.sh
+++ run_tests.sh
@@ -9,8 +9,8 @@ pub get
 
 pub run test --file-reporter=json:$filename.json || RESULT=1
 
-# pub run w_test_tools:xunit_parser -j "$filename.json" -t "$filename.xml"
+w_test_tools:xunit_parser -j "$filename.json" -t "$filename.xml"
 
-# rm "$filename.json"
+rm "$filename.json"
 
 exit $RESULT
