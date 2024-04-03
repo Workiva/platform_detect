75514099b21bb585673afd54e9ea15c1cdaca017
tusharmathur-wk <tushar.mathur@workiva.com>
2024-04-03 14:31:50 +0530

    FEDX-692 : Fix warnings

--- example/main.dart
+++ example/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: invalid_use_of_visible_for_testing_member
+// ignore_for_file: invalid_use_of_visible_for_testing_member, cast_nullable_to_non_nullable
 
 import 'dart:html';
 import 'package:platform_detect/platform_detect.dart';
